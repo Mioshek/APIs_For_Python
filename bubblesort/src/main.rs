bf428e609b68dceae590b07fa2653bc8c600cf59
Mioshek <milosz.szczygiel@interia.pl>
2022-12-16 00:06:15 +0100

    every loop string to int conversion changed and deleted

--- bubblesort/src/main.rs
+++ bubblesort/src/main.rs
@@ -4,7 +4,7 @@ fn get_console_args() -> Vec<String>{
     let mut args: Vec<_> = env::args().collect();
     args.remove(0);
     if args.len() == 0{
-        args.push("Empty Arr".to_string()); 
+        args.push("Empty Arr".to_string());
         return args;
     }
     else {
@@ -13,17 +13,22 @@ fn get_console_args() -> Vec<String>{
 }
 
 fn main() {
-    let mut to_sort = get_console_args();
-    for i in 0..to_sort.len() {
-        for j in 0..to_sort.len(){
-            let i_el:u64 = to_sort[i].parse().unwrap();
-            let j_el:u64 = to_sort[j].parse().unwrap();
-            if i_el > j_el {
-                let first_el:String = to_sort[i].to_owned();
-                to_sort[i] = to_sort[j].to_owned();
-                to_sort[j] = first_el;
+    let input_args = get_console_args();
+    if input_args != ["Empty Arr"]{
+        let mut to_sort: Vec<u64> = input_args.iter().flat_map(|x| x.parse()).collect();
+        for i in 0..to_sort.len() {
+            for j in 0..to_sort.len(){
+                if to_sort[i] > to_sort[j] {
+                    let first_el:u64 = to_sort[i];
+                    to_sort[i] = to_sort[j];
+                    to_sort[j] = first_el;
+                }
             }
         }
+        println!("{:?}", to_sort)
     }
-    println!("{:?}", to_sort)
+    else {
+        println!("{:?}", input_args)
+    }
+    
 }
--- bubblesort/src/test.py
+++ bubblesort/src/test.py
@@ -3,10 +3,11 @@ import pathlib
 def main(arr):
     cmd = ["cargo", "run", "--"] + [str(i) for i in arr]
     path = pathlib.Path(__file__).parent.resolve()
-    print (path)
 
     try:
         process:subprocess.run = subprocess.run(cmd, stdout=subprocess.PIPE, cwd=path)
         output_list:list = str(process.stdout)[3:-4].split("\\n")
         print (output_list)
     except Exception as exc: print(exc)
+    
+main([])
