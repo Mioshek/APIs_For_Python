0a1669912d9a9629c19e44ba7032001aab7f0349
Mioshek <milosz.szczygiel@interia.pl>
2022-12-16 15:30:05 +0100

    script returns value instead of printing and data transfer is via .txt file not console

--- bubblesort/src/main.rs
+++ bubblesort/src/main.rs
@@ -1,34 +1,28 @@
 use std::env;
+use std::fs;
 
-fn get_console_args() -> Vec<String>{
-    let mut args: Vec<_> = env::args().collect();
-    args.remove(0);
-    if args.len() == 0{
-        args.push("Empty Arr".to_string());
-        return args;
-    }
-    else {
-        return args;
-    }
+fn get_sequence(path:&String) -> Vec<String>{
+    
+    let raw_sequence = fs::read_to_string(path).expect("Content");
+    let sequence = raw_sequence.split(";").map(str::to_string).collect();
+    return sequence;
 }
 
 fn main() {
-    let input_args = get_console_args();
-    if input_args != ["Empty Arr"]{
-        let mut to_sort: Vec<u64> = input_args.iter().flat_map(|x| x.parse()).collect();
-        for i in 0..to_sort.len() {
-            for j in 0..to_sort.len(){
-                if to_sort[i] > to_sort[j] {
-                    let first_el:u64 = to_sort[i];
-                    to_sort[i] = to_sort[j];
-                    to_sort[j] = first_el;
-                }
+    let terminal_output: Vec<_> = env::args().collect();
+    let path = &terminal_output[1];
+    let input_args = get_sequence(path);
+    // println!("{:?}", input_args);
+    let mut to_sort: Vec<u64> = input_args.iter().flat_map(|x| x.parse()).collect();
+    for i in 0..to_sort.len() {
+        for j in 0..to_sort.len(){
+            if to_sort[i] > to_sort[j] {
+                let first_el:u64 = to_sort[i];
+                to_sort[i] = to_sort[j];
+                to_sort[j] = first_el;
             }
         }
-        println!("{:?}", to_sort)
     }
-    else {
-        println!("{:?}", input_args)
-    }
-    
+    let _file = fs::File::create(path); //clears old file's content
+    fs::write(path, format!("{:?}",to_sort)).expect("Unable to write file");
 }
--- bubblesort/src/test.py
+++ bubblesort/src/test.py
@@ -1,13 +1,26 @@
 import subprocess
 import pathlib
-def main(arr):
-    cmd = ["cargo", "run", "--"] + [str(i) for i in arr]
-    path = pathlib.Path(__file__).parent.resolve()
+path = pathlib.Path(__file__).parent.resolve()
+file_path = str(path) + "/tosort.txt"
+
+def create_file(arr) -> None:
+    with open(file_path, 'w+') as f:
+        [f.write(str(i)+";") for i in arr]
+
+def return_sorted() -> list:
+    with open(file_path, 'r') as f:
+        sorted = f.readline()
+    return sorted
+
+def main(arr) -> list:
+    create_file(arr)
+    cmd = ["cargo", "run", "--release", file_path]
 
     try:
-        process:subprocess.run = subprocess.run(cmd, stdout=subprocess.PIPE, cwd=path)
-        output_list:list = str(process.stdout)[3:-4].split("\\n")
-        print (output_list)
+        process:subprocess.run = subprocess.run(cmd, cwd=path)
     except Exception as exc: print(exc)
     
-main([])
+    return return_sorted()
+
+
+main([1,2,3,3535,61234,2453,4])
